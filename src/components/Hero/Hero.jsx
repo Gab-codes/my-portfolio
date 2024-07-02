ccad533c7cee7c72a99b8c72bab7a451192bc2b3
Temidayo Gabriel <dayotoru@gmail.com>
2024-07-02 08:38:14 -0400

    hero updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ export const Hero = () => {
   ref={ref}  id="hero" className={styles.container}>
     <motion.div 
     variants={{
-      hidden: {opacity: 0, y: -75},
+      hidden: {opacity: 0, y: 80},
       visible: {opacity: 1, y:0}
     }}
     initial = "hidden"
